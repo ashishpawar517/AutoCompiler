d658c15df470e20e37e3544448c90ec8f0df8e99
Aashish <pawarashish564@gmail.com>
2020-06-25 16:39:16 +0530

    added useful comments

--- RandomWriter.java
+++ RandomWriter.java
@@ -1,9 +1,9 @@
 import java.io.IOException;
 import java.io.RandomAccessFile;
-
+// this class is used for writing specific missing symbols in source java file
 public class RandomWriter {
     
-
+    //write to file to peek to certain position in file then write the certain data onto file
      void writeToFile(String filePath, String data, long position) throws IOException {
         RandomAccessFile file = new RandomAccessFile(filePath, "rw");
         file.seek(position);
