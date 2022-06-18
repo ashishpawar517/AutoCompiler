7aa35dd974faadace98681ecc633b036c1b0a7ee
Ashish <acpawar_b18@it.vjti.ac.in>
2022-06-18 16:06:05 +0530

    added support for command line args

--- src/main/java/com/ap/autocompiler/AutocompilerApplication.java
+++ src/main/java/com/ap/autocompiler/AutocompilerApplication.java
@@ -6,6 +6,6 @@ import java.io.IOException;
 public class AutocompilerApplication {
 
   public static void main(String[] args) throws IOException {
-    Main.main(null);
+    Main.main(args);
   }
 }
--- src/main/java/com/ap/autocompiler/main/Main.java
+++ src/main/java/com/ap/autocompiler/main/Main.java
@@ -4,14 +4,19 @@ import com.ap.autocompiler.compilation.GetErrorsFromSource;
 import com.ap.autocompiler.compilation.model.Error;
 import com.ap.autocompiler.errorfix.SolveErrorsFromSource;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 
 public class Main {
 
-  private static final String FILE_PATH =
+  private static String FILE_PATH =
     "src/main/resources/samples/HelloWorldProgram.java";
 
   public static void main(String[] args) throws IOException {
+    if (args != null && args.length > 0) {
+      FILE_PATH = args[0];
+    }
+    System.out.println(Arrays.toString(args));
     List<Error> errors = GetErrorsFromSource.compile(FILE_PATH);
     SolveErrorsFromSource.solve(errors, FILE_PATH);
   }
