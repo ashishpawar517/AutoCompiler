93cf2a439a887f756cc990fc2eda5572e0f5961d
Ashish <acpawar_b18@it.vjti.ac.in>
2022-06-18 16:05:47 +0530

    regex updated for class

--- src/main/java/com/ap/autocompiler/compilation/RegexParser.java
+++ src/main/java/com/ap/autocompiler/compilation/RegexParser.java
@@ -8,17 +8,11 @@ import java.util.stream.Collectors;
 public class RegexParser {
 
   private static final String JAVA_CLASS_REGEX =
-    "(((|public|final|abstract|private|static|protected)(\\s+))?(class)(\\s+)(\\w+)(<.*>)?(\\s+extends\\s+\\w+)?(<.*>)?(\\s+implements\\s+)?(.*)?(<.*>)?(\\s*))\\{$";
-
-  // private String interfaceRegExp =
-  //   "(((|public|final|abstract|private|static|protected)(\\s+))?interface(\\s+)(\\w+)(<.*>)?(\\s+extends\\s+\\w+)?(<.*>)?(\\s+implements\\s+)?(.*)?(<.*>)?(\\s*))\\{$";
-
-  // private String enumRegExp =
-  //   "((((public|final|private|static|protected)(\\s+))?enum(\\s+)(\\w+)?(\\s+implements\\s+\\w+)?(.*)?\\s*))\\{$";
+    "(private|public)\\s(class)\\s(\\w+)\\s?\\{(\\W*.*)*\\}$";
 
   public static boolean check(String filePath) throws IOException {
     return Pattern
-      .compile(JAVA_CLASS_REGEX)
+      .compile(JAVA_CLASS_REGEX, Pattern.MULTILINE)
       .matcher(
         FileUtils.readFile(filePath).stream().collect(Collectors.joining())
       )
